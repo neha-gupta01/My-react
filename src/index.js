aadee50cad0180ac8404432053ebb8d5ef45779d
neha-gupta01 <nehagupta96365@gmail.com>
2023-05-17 09:07:58 +0530

    React Styling Practice

--- src/index.js
+++ src/index.js
@@ -2,20 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const name = "Neha";
-const currentDate = new Date();
+const currentDate = new Date(2019, 1, 1, 17);
 const year = currentDate.getFullYear();
+const hour = currentDate.getHours();
 const img = "https://picsum.photos/200";
 
+let greeting;
+
+const customStyle = {
+  color: "",
+};
+
+if (hour < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (hour < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Night";
+  customStyle.color = "blue";
+}
 ReactDOM.render(
   <div>
-    <h1 className="heading">Hello World!</h1>
+    <h1 className="heading" style={customStyle}>
+      {greeting}
+    </h1>
     <h2>My favourite fruits </h2>
     <ul>
       <li>Mango</li>
       <li>Strawberry</li>
       <li>Grapes</li>
     </ul>
-    <img src={img}  alt="A Random" />
+    <img src={img} alt="A Random" />
     <p>Created by {name} </p>
     <p>Copyright {year} </p>
   </div>,
